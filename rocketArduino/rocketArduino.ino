c0a864a6b2af92e8a5ab18ba7801b129533384ab
berk ziya <berkziya1@gmail.com>
2020-12-31 19:02:43 +0300

    async and lower delay

--- rocketArduino/rocketArduino.ino
+++ rocketArduino/rocketArduino.ino
@@ -14,13 +14,27 @@ void setup() {
   if (!LoRa.begin(433E6)) {
     while (1);
   }
+  LoRa.setTxPower(20);
 }
 
 void loop() {
-  LoRa.beginPacket();
-  LoRa.write((uint8_t*)&data, sizeof(data));
-  LoRa.endPacket();
-  delay(5000);
+  if(runEvery(300)) {
+    LoRa.beginPacket();
+    LoRa.write((uint8_t*)&data, sizeof(data));
+    LoRa.endPacket(true);
+  }
 
   data.packageNo++;
+}
+
+boolean runEvery(unsigned long interval)
+{
+  static unsigned long previousMillis = 0;
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    return true;
+  }
+  return false;
 }
